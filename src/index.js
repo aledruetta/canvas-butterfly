63d8672bf490e87befae95856d87d445f2f092fd
Alejandro Druetta <aledruetta@gmail.com>
2018-01-18 21:44:15 -0200

    grid

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 const env = {
   width: 800,
   height: 600,
+  padding: 5,
+  gridWidth: 100,
+  gridHeight: 100,
 };
 
 const mov = {
   x0: 5,
   y0: 595,
-  thetaDeg: 35,
+  thetaDeg: 65,
   thetaRad: null,
-  v0: 85,
-  radius: 5,
+  v0: 95,
+  radius: 8,
 };
 
 const g = 9.8;
@@ -24,23 +27,61 @@ function grad2rad(deg) {
   return (Math.PI / 180) * deg;
 }
 
-function drawGrid() {
+function drawAxes() {
   ctx.beginPath();
 
-  ctx.moveTo(5, 0);
-  ctx.lineTo(5, 595);
+  ctx.moveTo(env.padding, 0);
+  ctx.lineTo(env.padding, env.height - env.padding);
   ctx.stroke();
 
   ctx.lineTo(800, 595);
   ctx.stroke();
+
+  ctx.moveTo(0, env.padding);
+  ctx.lineTo(env.padding, 0);
+  ctx.lineTo(10, env.padding);
+  ctx.stroke();
+
+  ctx.moveTo(795, 590);
+  ctx.lineTo(800, 595);
+  ctx.lineTo(795, 600);
+  ctx.stroke();
 }
 
-function drawmov() {
-  ctx.fillStyle = 'red';
+function drawGrid() {
+  ctx.beginPath();
+  ctx.save();
+  ctx.strokeStyle = 'gray';
 
+  // Vertical
+  for (let i = 0; i < (env.width / env.gridWidth); i++) {
+    ctx.beginPath();
+    ctx.moveTo((i * env.gridWidth) + env.padding, 0);
+    ctx.lineTo((i * env.gridWidth) + env.padding, env.height - env.padding);
+    ctx.stroke();
+  }
+
+  // Horizontal
+  for (let i = 1; i < (env.height / env.gridHeight); i++) {
+    ctx.beginPath();
+    ctx.moveTo(env.padding, env.height - env.padding - (i * env.gridHeight));
+    ctx.lineTo(env.width, env.height - env.padding - (i * env.gridHeight));
+    ctx.stroke();
+  }
+
+  ctx.restore();
+}
+
+function calcTraj() {
   const t = ((Date.now() / timeFactor) - tStart);
   const x = mov.x0 + (mov.v0 * Math.cos(mov.thetaRad) * t);
   const y = mov.y0 - (mov.v0 * Math.sin(mov.thetaRad) * t) + ((g * (t ** 2)) / 2);
+
+  return { x, y };
+}
+
+function drawMov(x, y) {
+  ctx.fillStyle = 'red';
 
   const startAngle = 0;
   const endAngle = 2 * Math.PI;
@@ -51,10 +92,12 @@ function drawmov() {
 }
 
 function loop() {
+  const pos = calcTraj();
   ctx.clearRect(0, 0, env.width, env.height);
 
+  drawAxes();
   drawGrid();
-  drawmov();
+  drawMov(pos.x, pos.y);
 
   window.requestAnimationFrame(loop);
 }
