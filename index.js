98ab501b007345e3d11dd385d309be35553840f6
Alejandro Druetta <aledruetta@gmail.com>
2018-01-18 01:40:49 -0200

    time factor

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const tela = {
-  width: 600,
-  height: 300,
+  width: 800,
+  height: 600,
 };
 
 const proj = {
-  x0: 0,
-  y0: 300,
-  thetaDeg: 30,
-  v0: 80,
-  radius: 3,
+  x0: 5,
+  y0: 595,
+  thetaDeg: 35,
+  v0: 85,
+  radius: 5,
 };
 
 const g = 9.8;
-const tStart = Date.now() / 1000;
+const timeFactor = 275;
+const tStart = Date.now() / timeFactor;
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
@@ -28,7 +29,7 @@ function draw() {
   ctx.clearRect(0, 0, tela.width, tela.height);
   ctx.fillStyle = 'red';
 
-  let t = ((Date.now() / 1000) - tStart);
+  let t = ((Date.now() / timeFactor) - tStart);
   let x = proj.x0 + proj.v0 * Math.cos(theta) * t;
   let y = proj.y0 - (proj.v0 * Math.sin(theta) * t) + ((g * (t ** 2)) / 2);
 
@@ -36,10 +37,10 @@ function draw() {
   const endAngle = 2 * Math.PI;
 
   ctx.beginPath();
-  ctx.arc(x, y, proj.radius, startAngle, endAngle, true);
+  ctx.arc(x, y, proj.radius, startAngle, endAngle);
   ctx.fill();
 
-  if (y > tela.height & x > 0) {
+  if (y >= proj.y0 & x > 0) {
     return
   }
 
